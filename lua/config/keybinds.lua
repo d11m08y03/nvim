a28e0528b6df81995b65fee5ecdad81b02b3fa88
Zakariyya Kurmally <zakariyyakurmally@gmail.com>
2026-08-09 16:50:34 +0400

    Add core editor options, keymaps, and autocmds

---
+++ lua/config/keybinds.lua
@@ -0,0 +1,22 @@
+vim.api.nvim_set_keymap("n", "<leader>w", "<cmd>:w<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>q", "<cmd>:q<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>h", "<cmd>:set nohlsearch<CR>", { noremap = true, silent = true })
+
+vim.keymap.set("v", "<", "<gv", { desc = "indent left, reselect" })
+vim.keymap.set("v", ">", ">gv", { desc = "indent right, reselect" })
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "move down in buffer with cursor centered" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "move up in buffer with cursor centered" })
+
+vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "moves lines down in visual selection" })
+vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "moves lines up in visual selection" })
+
+vim.keymap.set("n", "<leader>yp", function()
+	local path = vim.fn.expand("%:p")
+	if path == "" then
+		vim.notify("No file path (empty buffer)", vim.log.levels.WARN)
+		return
+	end
+	vim.fn.setreg("+", path)
+	vim.notify("Copied path: " .. path, vim.log.levels.INFO)
+end, { desc = "Copy current file path", noremap = true, silent = true })
---
+++ lua/config/options.lua
@@ -0,0 +1,27 @@
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.wrap = false
+
+vim.opt.signcolumn = "yes"
+
+vim.opt.winborder = "rounded"
+vim.opt.splitright = true
+
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.opt.foldcolumn = "0" -- Set to "1" if you want to see a side column for folds
+vim.opt.foldlevel = 99   -- Keep most folds open by default
+vim.opt.foldlevelstart = 99
+
+-- za	Toggle the current fold (open if closed, close if open).[2][3][4][5]
+-- zo	Open the current fold.
+-- zc	Close the current fold.
+-- zR	Open all folds in the entire file.[1][2][3]
+-- zM	Close all folds in the entire file.
+-- zf	Create a manual fold (select text in Visual mode first).[2][3][6]
+-- zd	Delete the fold at the cursor.[2][7]
